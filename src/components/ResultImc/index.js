b8851c6976443eaf549b207bdb57ae05a18e6528
Mikael Tavares <mikael.tavares@unigranrio.br>
2021-06-12 00:09:32 -0300

    Adicionando botão de compartilhar com a API de share do react native.

--- src/components/ResultImc/index.js
+++ src/components/ResultImc/index.js
@@ -1,10 +1,26 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity, Share } from "react-native"
 import styles from "./style";
 
 export default function ResultImc(props){
+
+    const onShare = async () => {
+        const result = await Share.share({
+            message: "Meu IMC hoje é: " + props.resultImc
+        })
+    }
+
     return (
         <View style={styles.resultImc}>
+            <View style={styles.boxShareButton}>
+                { props.resultImc != null ? //Se o props.resultImc for nulo, retorna o touchableopacity, se não retorna uma view vazia. 
+                <TouchableOpacity style={styles.share}
+                onPress={onShare}>
+                    <Text style={styles.sharedText}>Compartilhar</Text>
+                </TouchableOpacity>
+                : <View/>
+                }  
+            </View>
             <Text style={styles.information}>{props.messageResultImc}</Text>
             <Text style={styles.numberImc}>{props.resultImc}</Text>
         </View>
